252f40132fd279c4cf7b7f94770caa14cb3ea4e2
fundaBa <2026217@student.cct.ie>
2026-05-31 03:01:42 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,20 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 st.title("Agricultural Production Dashboard")
 
-DATA_URL = "ml_livestock_and_milk.csv"
-
-def load_data():
-    data = pd.read_csv(DATA_URL)
-    return data
-
-data_load_state = st.text("Loading data...")
-
-data = load_data()
-
-data_load_state.text("Loading data... Done!")
-
-st.subheader("Raw Dataset")
-
-st.write(data)
