b80fd58346414e270643163dc46eaebecb197de7
fundaBa <2026217@student.cct.ie>
2026-05-31 20:17:19 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -2,23 +2,25 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title("Agricultural Production Dashboard")
+st.title("Livestock and Raw Milk Dashboard")
 
-DATA_URL = ('https://raw.githubusercontent.com/'
+DATA_URL = (
+    'https://raw.githubusercontent.com/'
     'fundaBa/ca2-dashboard/main/FAOSTAT_livestock_and_milk.csv'
 )
-
 
 def load_data():
     data = pd.read_csv(DATA_URL)
     return data
 
-data_load_state = st.text("Loading Data...")
-
 data = load_data()
 
-data_load_state.text("Loading Data...Done!")
-
-st.subheader("Raw Data")
+st.subheader("Livestock and Milk Dataset")
 
 st.write(data)
+
+st.subheader("Dataset Information")
+
+st.write(f"Number of Records: {data.shape[0]}")
+st.write(f"Number of Variables: {data.shape[1]}")
+st.write(f"Countries Included: {data['Area'].nunique()}")
