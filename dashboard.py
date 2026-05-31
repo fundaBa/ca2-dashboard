6762f08b478a47e2cdfdf13a6b4d9d8dedb4f1ba
fundaBa <2026217@student.cct.ie>
2026-05-31 03:07:14 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -4,3 +4,21 @@ import numpy as np
 
 st.title("Agricultural Production Dashboard")
 
+DATA_URL = (
+    "https://raw.githubusercontent.com/"
+    "CCT-Dublin/ca2-sem-1-fundaBa/main/FAOSTAT_livestock_and_milk.csv"
+)
+
+def load_data():
+    data = pd.read_csv(DATA_URL)
+    return data
+
+data_load_state = st.text("Loading Data...")
+
+data = load_data()
+
+data_load_state.text("Loading Data...Done!")
+
+st.subheader("Raw Data")
+
+st.write(data)
